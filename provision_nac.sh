7f021796f895af76e9013555d9b002e252307faf
Sachin Gaikwad <sachin.gaikwad@nasuni.com>
2022-11-22 18:20:05 +0530

    Updated LOG_File for corntab

--- provision_nac.sh
+++ provision_nac.sh
@@ -19,7 +19,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 set -e
 DATE_WITH_TIME=$(date "+%Y%m%d-%H%M%S")
 LOG_FILE=provision_nac_$DATE_WITH_TIME.log
-
+(
 START=$(date +%s)
 {
 
@@ -589,3 +589,5 @@ echo "INFO ::: Total execution Time ::: $DIFF"
         exit 0
     echo "INFO ::: Failed NAC Povisioning"
 }
+)2>&1 | tee $LOG_FILE
+
