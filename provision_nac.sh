ed6a6521034c372e4daa89913d83c952c4671059
Aniket <abedare@nasuni.com>
2023-02-20 12:31:21 +0530

    updated branch name

--- provision_nac.sh
+++ provision_nac.sh
@@ -507,7 +507,7 @@ get_subnets(){
 
 ###### START - EXECUTION ######
 ### GIT_BRANCH_NAME decides the current GitHub branch from Where Code is being executed
-GIT_BRANCH_NAME="CTPROJECT-457"
+GIT_BRANCH_NAME="CTPROJECT-256"
 if [[ $GIT_BRANCH_NAME == "" ]]; then
     GIT_BRANCH_NAME="main"
 fi
